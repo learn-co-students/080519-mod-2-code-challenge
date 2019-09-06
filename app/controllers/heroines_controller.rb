1875969530354dfaed1e57d6e1ba73235170bd8a
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-09-06 12:25:29 -0500

    ahhhh

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,20 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show 
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(params[:id])
+    redirect_to heroine_path(@heroine)
+  end
+
+  
+  
 end
