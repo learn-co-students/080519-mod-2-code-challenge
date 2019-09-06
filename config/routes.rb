77999e67ec037b93f7479469a1549b5719d0632e
Anais Ahmed <anaismahmed@uchicago.edu>
2019-09-06 11:53:38 -0500

    Made a home page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :powers, only: [:index, :show]
   get '/heroines/search', to: 'heroines#search'
   resources :heroines, only: [:index, :show, :new, :create]  
+  get '/', to: 'static#home'
 end
