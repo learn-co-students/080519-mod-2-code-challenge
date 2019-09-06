4c149d19692a04ca4b4ba4034bb20c4fbaa1895c
Syd <green@Syds-MacBook-Air-2.local>
2019-09-06 12:24:07 -0500

    Syd's second code challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,26 @@
 class HeroinesController < ApplicationController
+  
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = @heroine.power
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    @powers = Power.all
+    redirect_to @heroine
+  end
+
+  def heroine_params
+    params.permit(:name, :super_name, :power_id)
+  end
+
 end
